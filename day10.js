746de19850287c5ca25b1c717206dc21943dd4ac
Manas <official.manastiwari2101@gmail.com>
2025-10-28 11:32:17 +0530

    notes about random math function

---
+++ day10.js
@@ -0,0 +1,8 @@
+// ############### Random ##############
+// if i want to generate random numbers between any range then take min and max value 
+// const min=1
+// const max=6
+// to generate random values we use a formula (max-min +1) this will generate numbers between min and max including both
+// let num=Math.floor((Math.random()*(max-min+1))+min) here i used Math.floor to round off the decimal values to nearest lower integer
+// and i multiplied Math.random() with (max-min+1) to get the values in range and then added min to shift the range from 0 to min
+// console.log(num) 
